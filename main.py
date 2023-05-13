7b331bca3a5dfc00843c773a9b5ecbb740eb4b5e
marwan-usama-z15 <marwanusama1513@gmail.com>
2023-05-13 20:08:12 +0300

    start of my part

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 a1=[]
 board = [    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
              [' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -66,3 +67,48 @@ def check_winner(board):
 
     return 0
 # my code part
+
+def minimax(board, depth, is_maximizing, first_time=True):
+    result = check_winner(board)
+    if depth == 0 or result != 1:
+        return result, 1, 1
+
+    if is_maximizing:
+        final_score = -math.inf
+        for i in range(0, 6):
+            for j in range(0, 7):
+                if board[i][j] == ' ':
+                    if (i == 5 and (j == 0 or j == 1 or j == 2 or j == 3 or j == 4 or j == 5 or j == 6)) or (
+                            i != 5 and board[i + 1][j] != ' '):
+                        board[i][j] = 'X'
+                        scores = minimax(board, depth - 1, False, False)
+                        score = scores[0]
+                        board[i][j] = ' '
+                        if score > final_score:
+                            final_score = score
+                            final_i, final_j = i, j
+                        a1.append(final_score)
+
+        if first_time:
+            board[final_i][final_j] = 'O'
+        return final_score, final_i, final_j
+    else:
+        final_score = math.inf
+        final_i, final_j = None, None
+        for i in range(0, 6):
+            for j in range(0, 7):
+                if board[i][j] == ' ':
+                    if (i == 5 and (j == 0 or j == 1 or j == 2 or j == 3 or j == 4 or j == 5 or j == 6)) or (
+                            i != 5 and board[i + 1][j] != ' '):
+                        board[i][j] = 'O'
+                        scores = minimax(board, depth - 1, True, False)
+                        score = scores[0]
+                        board[i][j] = ' '
+                        if score < final_score:
+                            final_score = score
+                            final_i, final_j = i, j
+                        a1.append(final_score)
+
+        if first_time:
+            board[final_i][final_j] = 'O'
+        return final_score, final_i, final_j
